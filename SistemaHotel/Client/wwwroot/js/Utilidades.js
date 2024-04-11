e4a550e9f58db01470c36f6c18902b0e491b9b24
mathiaspar2000 <mathiaspar2000@gmail.com>
2024-04-11 17:34:12 -0500

    Agregar archivos de proyecto.

---
+++ SistemaHotel/Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,26 @@
+﻿// wwwroot/js/interop.js
+
+window.DescargarCSV = (nombreArchivo, contenidoBase64) => {
+    const byteArray = base64ToArrayBuffer(contenidoBase64);
+    const blob = new Blob([byteArray], { type: 'text/csv' });
+
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = nombreArchivo;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+};
+
+function base64ToArrayBuffer(base64) {
+    const binaryString = window.atob(base64);
+    const length = binaryString.length;
+    const bytes = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
